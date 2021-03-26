a08d17cc1996469afc2fcec78e9c4111d9b86d45
Joanna Chądzyńska <jmalek87@gmail.com>
2021-03-26 15:23:14 +0100

    update handle errors method

--- jwt-auth/server/controllers/authController.js
+++ jwt-auth/server/controllers/authController.js
@@ -1,22 +1,47 @@
+import jwt from 'jsonwebtoken';
 import User from '../models/User.js';
 
 // handle errors
 const handleErrors = (err) => {
-	// console.log(err.message, err.code);
+	console.log(err.message, err.code);
 	let errors = { email: '', password: '' };
 
-	// duplicate error code
+	// incorrect email
+	if (err.message === 'incorrect email') {
+		errors.email = 'That email is not registered';
+	}
+
+	// incorrect password
+	if (err.message === 'incorrect password') {
+		errors.password = 'That password is incorrect';
+	}
+
+	// duplicate email error
 	if (err.code === 11000) {
-		errors.email = 'That email is already registered';
+		errors.email = 'that email is already registered';
 		return errors;
 	}
+
 	// validation errors
 	if (err.message.includes('user validation failed')) {
+		// console.log(err);
 		Object.values(err.errors).forEach(({ properties }) => {
+			// console.log(val);
+			// console.log(properties);
 			errors[properties.path] = properties.message;
 		});
 	}
+
 	return errors;
+};
+
+const maxAge = 3 * 24 * 60 * 60;
+
+// generate token
+const generateAccessToken = (payload) => {
+	return jwt.sign(payload, process.env.JWT_TOKEN_SECRET, {
+		expiresIn: '3d',
+	});
 };
 
 /**
@@ -33,11 +58,30 @@ export const signup_post = async (req, res) => {
 			password,
 			name,
 		});
-		res.status(201).json(user);
-		res.send('signup new user');
+
+		const accessToken = generateAccessToken({ id: user._id });
+
+		const refreshToken = jwt.sign(
+			{ id: user._id },
+			process.env.JWT_REFRESH_TOKEN_SECRET
+		);
+
+		res.cookie('access_token', accessToken, {
+			httpOnly: true,
+			maxAge: 259200000,
+		});
+		res.cookie('refresh_token', refreshToken, {
+			httpOnly: true,
+			maxAge: 267840000,
+		});
+
+		res.status(201).json({
+			user: user._id,
+			accessToken,
+		});
 	} catch (error) {
 		const errors = handleErrors(error);
-		res.status(400).json(errors);
+		res.status(400).send(errors);
 	}
 };
 
@@ -56,8 +100,14 @@ export const logout_post = (req, res) => {
 	res.send('User logout');
 };
 
-export const token_post = (req, res) => {
+export const token_post = async (req, res) => {
 	res.send('refresh token');
+	try {
+		const { refreshToken } = req.body;
+	} catch (error) {
+		console.error(error);
+		return res.status(500).json({ error: 'Internal Server Error!' });
+	}
 };
 
 /**
