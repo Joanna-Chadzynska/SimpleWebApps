764ea81f9db12559b28c544428a6edfafccabda2
Joanna Chądzyńska <jmalek87@gmail.com>
2021-04-03 20:03:21 +0200

    update navbar: add logout button and conditional rendering

--- jwt-auth/client/src/components/Navbar.tsx
+++ jwt-auth/client/src/components/Navbar.tsx
@@ -1,39 +1,36 @@
+import axios from 'axios';
 import React from 'react';
-import { Nav, Navbar } from 'react-bootstrap';
-import { NavLink } from 'react-router-dom';
+import { Button, Nav, Navbar } from 'react-bootstrap';
+import { NavLink, useHistory } from 'react-router-dom';
 
 export interface NavbarContainerProps {}
 
 const NavbarContainer: React.SFC<NavbarContainerProps> = () => {
+	const history = useHistory();
+	const currentUser = localStorage.getItem('currentUser');
+
+	const logout = async () => {
+		const resp = await axios.get('/api/auth/logout');
+		localStorage.removeItem('currentUser');
+		window.location.reload();
+		history.push('/');
+		return resp;
+	};
 	return (
 		<Navbar collapseOnSelect expand='lg' bg='dark' variant='dark'>
 			<Navbar.Brand href='/'>Auth.</Navbar.Brand>
 			<Navbar.Toggle aria-controls='responsive-navbar-nav' />
 			<Navbar.Collapse id='responsive-navbar-nav'>
-				{/* <Nav className='mr-auto'>
-					<Nav.Link href='#features'>Features</Nav.Link>
-					<Nav.Link href='#pricing'>Pricing</Nav.Link>
-					<NavDropdown title='Dropdown' id='collasible-nav-dropdown'>
-						<NavDropdown.Item href='#action/3.1'>Action</NavDropdown.Item>
-						<NavDropdown.Item href='#action/3.2'>
-							Another action
-						</NavDropdown.Item>
-						<NavDropdown.Item href='#action/3.3'>Something</NavDropdown.Item>
-						<NavDropdown.Divider />
-						<NavDropdown.Item href='#action/3.4'>
-							Separated link
-						</NavDropdown.Item>
-					</NavDropdown>
-				</Nav> */}
 				<Nav>
-					<NavLink to='/signin'>Sign in</NavLink>
-					&nbsp; &nbsp; &nbsp;
-					<NavLink to='/signup'>Sign up</NavLink>
-					{/* <Navbar.Collapse className='justify-content-end'>
-						<Navbar.Text>
-							Signed in as: <a href='#login'>Mark Otto</a>
-						</Navbar.Text>
-					</Navbar.Collapse> */}
+					{currentUser ? (
+						<Button onClick={logout}>Sign out</Button>
+					) : (
+						<>
+							<NavLink to='/signin'>Sign in</NavLink>
+							&nbsp; &nbsp; &nbsp;
+							<NavLink to='/signup'>Sign up</NavLink>
+						</>
+					)}
 				</Nav>
 			</Navbar.Collapse>
 		</Navbar>
