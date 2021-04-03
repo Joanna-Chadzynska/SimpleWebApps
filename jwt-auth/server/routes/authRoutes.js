56a5594f44cd53b64d2d70ba346f8314e946c09d
Joanna Chądzyńska <jmalek87@gmail.com>
2021-04-03 20:02:50 +0200

    upgate routes

--- jwt-auth/server/routes/authRoutes.js
+++ jwt-auth/server/routes/authRoutes.js
@@ -1,17 +1,17 @@
 import { Router } from 'express';
 import {
-    login_post,
-    logout_post,
-    signup_post,
-    token_post,
-    user_get
+	login_post,
+	logout_get,
+	signup_post,
+	token_post,
+	user_get,
 } from '../controllers/authController.js';
 
 const router = Router();
 
 router.post('/signup', signup_post);
 router.post('/login', login_post);
-router.post('/logout', logout_post);
+router.get('/logout', logout_get);
 router.post('/token', token_post);
 router.get('/user', user_get);
 
