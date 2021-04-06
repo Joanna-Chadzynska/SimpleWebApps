9798b2e90ac328e48b2a2567901bc128b59a7bde
Joanna Chądzyńska <jmalek87@gmail.com>
2021-04-06 21:17:52 +0200

    update api.ts file

--- jwt-auth/client/src/api/api.ts
+++ jwt-auth/client/src/api/api.ts
@@ -1,17 +1,49 @@
 import axios from 'axios';
 
-export const axiosAuth = axios.create({
-	baseURL: `http://localhost:4000/api`,
-	headers: {
-		Authorization: 'bearer ' + window.localStorage.getItem('token'),
-		'Content-Type': 'application/json',
-		Accepts: 'application/json',
-	},
+const baseURL = `http://localhost:4000/api`;
+
+export const axiosInstance = axios.create({
+	baseURL,
 });
 
-export const axiosNoAuth = axios.create({
-	baseURL: `http://localhost:4000/api`,
-	headers: {
-		'Content-Type': 'application/json',
+//request interceptor to add the auth token header to requests
+axios.interceptors.request.use(
+	(config) => {
+		const accessToken = localStorage.getItem('accessToken');
+		if (accessToken) {
+			config.headers['x-auth-token'] = accessToken;
+		}
+		return config;
 	},
-});
+	(error) => {
+		Promise.reject(error);
+	}
+);
+
+//response interceptor to refresh token on receiving token expired error
+axios.interceptors.response.use(
+	(response) => {
+		return response;
+	},
+	function (error) {
+		const originalRequest = error.config;
+		let refreshToken = localStorage.getItem('refreshToken');
+		if (
+			refreshToken &&
+			error.response.status === 401 &&
+			!originalRequest._retry
+		) {
+			originalRequest._retry = true;
+			return axios
+				.post(`${baseURL}/auth/refresh_token`, { refreshToken: refreshToken })
+				.then((res) => {
+					if (res.status === 200) {
+						localStorage.setItem('accessToken', res.data.accessToken);
+						console.log('Access token refreshed!');
+						return axios(originalRequest);
+					}
+				});
+		}
+		return Promise.reject(error);
+	}
+);
