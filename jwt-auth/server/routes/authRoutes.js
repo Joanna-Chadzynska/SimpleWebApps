b592ffe61e88f4d1c1b5663532aa8bd3e57c3d17
Joanna Chądzyńska <jmalek87@gmail.com>
2021-04-06 21:16:28 +0200

    rename refresh token route

--- jwt-auth/server/routes/authRoutes.js
+++ jwt-auth/server/routes/authRoutes.js
@@ -2,8 +2,8 @@ import { Router } from 'express';
 import {
 	login_post,
 	logout_get,
+	refresh_token,
 	signup_post,
-	token_post,
 	user_get,
 } from '../controllers/authController.js';
 
@@ -12,7 +12,7 @@ const router = Router();
 router.post('/signup', signup_post);
 router.post('/login', login_post);
 router.get('/logout', logout_get);
-router.post('/token', token_post);
+router.post('/refresh-token', refresh_token);
 router.get('/user', user_get);
 
 export default router;
