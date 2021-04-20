71f83e94f2bd4c51643459dc24bbbccc30368ba5
Joanna Chądzyńska <jmalek87@gmail.com>
2021-04-20 21:36:09 +0200

    remove unused code

--- jwt-auth/server/routes/postsRoutes.js
+++ jwt-auth/server/routes/postsRoutes.js
@@ -1,25 +1,9 @@
 import { Router } from 'express';
 import { posts_get } from '../controllers/postsController.js';
 import requireAuth from '../middleware/authMiddleware.js';
+
 const router = Router();
 
 router.get('/', requireAuth, posts_get);
-// app.get('/api/posts', authenticateToken, (req, res) => {
-// 	res.send(posts);
-// });
-function authenticateToken(req, res, next) {
-	const authHeader = req.headers['authorization'];
-
-	const token = authHeader && authHeader.split(' ')[1];
-	// const token = req.cookies.JWT;
-	// console.log(token);
-	if (token === null) return res.sendStatus(401);
-
-	jwt.verify(token, process.env.TOKEN_SECRET, (err, user) => {
-		if (err) return res.sendStatus(403);
-		req.user = user;
-		next();
-	});
-}
 
 export default router;
