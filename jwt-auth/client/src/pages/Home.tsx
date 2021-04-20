4de86512cf0d8cc2579c52e30a377eb954df5212
Joanna Chądzyńska <jmalek87@gmail.com>
2021-04-20 21:37:57 +0200

    remove get posts from landing and add it to home page

--- jwt-auth/client/src/pages/Home.tsx
+++ jwt-auth/client/src/pages/Home.tsx
@@ -1,9 +1,36 @@
-import React from 'react';
+import axios from 'axios';
+import React, { useEffect } from 'react';
 import { Container } from 'react-bootstrap';
+import { axiosPosts } from '../api/api';
 
 export interface HomeProps {}
 
 const Home: React.SFC<HomeProps> = () => {
+	const getPosts = async () => {
+		try {
+			const res = await axiosPosts.get('/posts', { withCredentials: true });
+		} catch (error) {
+			console.log(error);
+		}
+	};
+	useEffect(() => {
+		getPosts();
+		return () => {};
+	}, []);
+
+	const refreshToken = async () => {
+		if (!document.cookie.includes('_csrf')) return;
+		try {
+			const { data } = await axios.post(
+				'/api/auth/refresh-token',
+				{},
+				{
+					withCredentials: true,
+				}
+			);
+		} catch (error) {}
+	};
+
 	return <Container>home page</Container>;
 };
 
--- jwt-auth/client/src/pages/Landing.tsx
+++ jwt-auth/client/src/pages/Landing.tsx
@@ -1,22 +1,9 @@
-import axios from 'axios';
-import React, { useEffect } from 'react';
+import React from 'react';
 import { Container } from 'react-bootstrap';
 
 export interface LandingProps {}
 
 const Landing: React.SFC<LandingProps> = () => {
-	const getPosts = async () => {
-		try {
-			const res = await axios.get('/api/posts');
-			console.log(res.data);
-		} catch (error) {
-			console.log(error);
-		}
-	};
-	useEffect(() => {
-		getPosts();
-		return () => {};
-	}, []);
 	return <Container>Landing page</Container>;
 };
 
