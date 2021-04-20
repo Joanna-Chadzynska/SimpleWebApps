f0f10fb0253f5d7cacdc7fea110f5816035674ce
Joanna Chądzyńska <jmalek87@gmail.com>
2021-04-20 21:35:43 +0200

    update auth middleware

--- jwt-auth/server/middleware/authMiddleware.js
+++ jwt-auth/server/middleware/authMiddleware.js
@@ -2,16 +2,14 @@ import jwt from 'jsonwebtoken';
 
 const requireAuth = (req, res, next) => {
 	const token = req.cookies.access_token;
-
-	// check jwt exists and verified
+	// 	// check jwt exists and verified
 	if (!token) return res.sendStatus(401);
-	if (token) {
-		jwt.verify(token, process.env.JWT_TOKEN_SECRET, (err, decodedToken) => {
-			if (err) return res.status(403);
-			console.log(decodedToken);
-			next();
-		});
-	}
+
+	jwt.verify(token, process.env.JWT_TOKEN_SECRET, (err, decodedToken) => {
+		if (err) return res.status(403);
+		// console.log(decodedToken);
+		next();
+	});
 };
 
 // check current user
